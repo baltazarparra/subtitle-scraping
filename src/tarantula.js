658086caba2509e2e026f1839103814446491164
btrparra <btparra@esapiens.com.br>
2019-02-19 20:41:35 -0300

    Bater na rota de login para aproveitar seletores mais definidos

--- src/tarantula.js
+++ src/tarantula.js
@@ -1,29 +1,12 @@
 const fs = require('fs')
 const puppeteer = require('puppeteer')
 
-const scrape = async () => {
+async function run() {
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
-  console.log('Open browser...')
-  await page.goto('http://legendas.tv/')
-  console.log('Entering in site...')
-  await page.waitFor('.bigtitle')
-
-  console.log('Scraping data...')
-  const result = await page.evaluate(() => {
-    return document.querySelector('.bigtitle').innerText
-  })
-
-  console.log('Closing browser...')
-  await browser.close()
-
-  return result
+  await page.goto('http://legendas.tv/login')
+  await page.screenshot({ path: 'login.png'})
+  browser.close()
 }
 
-scrape()
-  .then((result) => {
-    console.log('Saving data...')
-    fs.appendFile('simpsons-subtitle.txt', result, (err) => {
-      if(err) console.log(err)
-    })
-  })
+run()

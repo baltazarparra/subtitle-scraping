a77a757ad8d1a5bc7d3d1912a5b35eece30490c3
btrparra <btparra@esapiens.com.br>
2019-02-19 20:48:35 -0300

    Logar no site com as credenciais sugeridas

--- src/tarantula.js
+++ src/tarantula.js
@@ -5,7 +5,17 @@ async function run() {
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
   await page.goto('http://legendas.tv/login')
+
+  console.log('Entering in site...')
+  await page.screenshot({ path: 'enter.png'})
+
+  await page.type('#UserUsername', 'fliperapp')
+  await page.type('#UserPassword', '123456')
+  await page.click('#UserLoginForm > button')
+
+  console.log('Loging...')
   await page.screenshot({ path: 'login.png'})
+
   browser.close()
 }
 

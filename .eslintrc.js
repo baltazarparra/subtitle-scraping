96b415f75405bb73c4b7bf1cb0359c8a5bffa7f1
btrparra <btparra@esapiens.com.br>
2019-02-19 19:03:20 -0300

    Criar estrutura inicial do scraping e instalar dependencias

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true,
+      "amd": true
+  },
+  "extends": [
+    "airbnb"
+  ],
+  "parserOptions": {
+      "ecmaVersion": 2017
+  },
+  "rules": {
+      "no-console": "off",
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          "trailingComma": "es5",
+          "singleQuote": true,
+          "printWidth": 120
+        }
+      ],
+  }
+};

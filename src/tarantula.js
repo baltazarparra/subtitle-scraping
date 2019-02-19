2de10f3049728aeed96f2d24311962e824c7e3cf
btrparra <btparra@esapiens.com.br>
2019-02-19 19:31:42 -0300

    Adicionar logs para acompanhar a execuçao

--- src/tarantula.js
+++ src/tarantula.js
@@ -4,13 +4,17 @@ const puppeteer = require('puppeteer')
 const scrape = async () => {
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
+  console.log('Open browser...')
   await page.goto('http://legendas.tv/')
+  console.log('Entering in site...')
   await page.waitFor('.bigtitle')
 
+  console.log('Scraping data...')
   const result = await page.evaluate(() => {
     return document.querySelector('.bigtitle').innerText
   })
 
+  console.log('Closing browser...')
   await browser.close()
 
   return result
@@ -18,6 +22,7 @@ const scrape = async () => {
 
 scrape()
   .then((result) => {
+    console.log('Saving data...')
     fs.appendFile('simpsons-subtitle.txt', result, (err) => {
       if(err) console.log(err)
     })

8ce3ab75c6f3c96682840e04bae2195ce833f6de
btrparra <btparra@esapiens.com.br>
2019-02-20 06:09:47 -0300

    Retornar json com dados extraidos

--- src/tarantula.js
+++ src/tarantula.js
@@ -1,18 +1,21 @@
 const entries = require('../entries')
 const puppeteer = require('puppeteer')
+const fs = require('fs')
 
 const scraping = async () => {
+
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
   await page.goto(`${entries.url}${entries.loginRoute}`)
 
-  console.log('Entering in site...')
+  console.log('Open login page...')
   await page.screenshot({ path: '../screens/enter.png'})
 
   await page.type('#UserUsername', entries.username)
   await page.type('#UserPassword', entries.password)
 
-  console.log('Loging...')
+  console.log('Enter login info...')
+
   await Promise.all([
     page.click('#UserLoginForm > button'),
     page.waitForNavigation()
@@ -21,16 +24,38 @@ const scraping = async () => {
   console.log('Loged...')
   await page.screenshot({ path: '../screens/login.png'})
 
-  console.log('Searching...')
   await page.goto(`${entries.url}${entries.searchRoute}${entries.searchTerm}`)
 
-  console.log('Searched...')
+  console.log('Searching term...')
   await page.screenshot({ path: '../screens/searched.png'})
+
+  const results = await page.evaluate(() => {
+
+    const subtitlesList = Array.from(document.querySelectorAll('.gallery > article > div'))
+    const subtitles = []
+    subtitlesList.map((item) => {
+      const name = item.querySelector('p > a').innerText
+      const link = item.querySelector('p > a').href
+      const img = item.querySelector('img').src
+      subtitles.push({
+        name,
+        link,
+        img
+      })
+    })
+
+    return subtitles
+  })
 
   await browser.close()
 
+  return results
 }
 
 scraping()
-  .then((item) => console.log('Done'))
+  .then((item) => {
+    fs.appendFile('subtitles.json', JSON.stringify(item), (err) => {
+      if(err) console.log(err)
+    })
+  })
   .catch((e) => console.log(e))
